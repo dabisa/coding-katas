57c7860acef9808e2d4b43ac31eb5517d6b14fb0
Dabisa Kelava <dabisa.kelava@gmail.com>
2018-02-14 23:25:28 +0000

    stage 2

--- file-logger-kata/src/main/java/FileLogger.java
+++ file-logger-kata/src/main/java/FileLogger.java
@@ -1,4 +1,34 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FileLogger{
-    public void log(String message) {
+
+    public FileLogger() {
+        localDate = LocalDate.now();
     }
+
+    public FileLogger(LocalDate date) {
+        localDate = date;
+    }
+
+    public String getFileName() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String strDate = localDate.format(formatter);
+        return "log" + strDate + ".txt";
+    }
+
+    public void log(String message) throws IOException {
+        String filename = getFileName();
+        File file = new File(filename);
+        file.createNewFile();
+        Writer writer = new FileWriter(file, true);
+        writer.append(message + "\n");
+        writer.flush();
+    }
+
+    private LocalDate localDate;
 }
--- file-logger-kata/src/test/java/FileLoggerTest.java
+++ file-logger-kata/src/test/java/FileLoggerTest.java
@@ -1,22 +1,64 @@
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class FileLoggerTest {
+
     @Test
-    public void test_class_existence(){
-        try {
-            Class.forName("FileLogger");
-        } catch(ClassNotFoundException e) {
-            Assert.fail("Class named FileLogger should exist");
-        }
+    public void test_file_creation() throws IOException {
+        // Context
+        FileLogger logger = new FileLogger();
+        Path path = Paths.get(logger.getFileName());
+        Files.delete(path);
+        Assert.assertFalse(Files.exists(path));
+
+        // Business logic
+        logger.log("Hello");
+
+        // Asserts
+        Assert.assertTrue(Files.exists(path));
     }
 
     @Test
-    public void test_method_existence(){
-        try {
-            FileLogger.class.getDeclaredMethod("log", String.class);
-        } catch(NoSuchMethodException e) {
-            Assert.fail("Class named FileLogger should have method called log with string parameter");
+    public void test_if_message_is_appended() throws IOException {
+        // Context
+        FileLogger logger = new FileLogger();
+        Path path = Paths.get(logger.getFileName());
+        String message = "My Message";
+
+        // Business logic
+        List<String> linesBefore = Files.readAllLines(path);
+        logger.log(message);
+        List<String> linesAfter = Files.readAllLines(path);
+
+        // Asserts
+        for(int i = 0; i < linesBefore.size(); ++i) {
+           Assert.assertEquals(linesBefore.get(i), linesAfter.get(i));
         }
+        Assert.assertEquals(linesBefore.size() + 1, linesAfter.size());
+        Assert.assertEquals(message, linesAfter.get(linesAfter.size() - 1));
+    }
+
+    @Test
+    public void test_if_name_is_correct() throws IOException {
+        // Context
+        LocalDate date = LocalDate.of(2018,02,14);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String strDate = date.format(formatter);
+        String expectedFilename = "log" + strDate + ".txt";
+
+        // Business logic
+        FileLogger fileLogger = new FileLogger(date);
+        String filename = fileLogger.getFileName();
+
+        // Asserts
+        Assert.assertEquals(expectedFilename, filename);
     }
 }
